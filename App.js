6b09ed5249509b024b66895d12fb10762f153ced
Jin-MacBook <taejin7824@gmail.com>
2021-02-26 19:32:47 +0900

    #1.4 Getting the Weather
    Codes for getting weather using openweathermap api

--- App.js
+++ App.js
@@ -2,11 +2,24 @@
 import React from "react";
 import { Alert } from "react-native";
 
-import Loading from "./Loading";
+import axios from "axios";
 import * as Location from "expo-location";
+
+import Loading from "./Loading";
+
+const API_KEY = "20e3b7d6e432fb24d46adc288538bc70";
 
 export default class extends React.Component {
   state = { isLoading: true };
+
+  getWeather = async (lat, long) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`
+    );
+
+    console.log(data);
+  };
+
   getLocation = async () => {
     let { status } = await Location.requestPermissionsAsync();
     if (status !== "granted") {
@@ -16,6 +29,7 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
 
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
       console.log(latitude, longitude);
     }
