632aee404f39055514f6772ee17d1070f0b1df2a
Jin-MacBook <taejin7824@gmail.com>
2021-02-26 18:39:07 +0900

    #1.1 Loading Screen
    I made the Loading Screen Component using RN

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Getting the weather </Text>
+    </View>
+  );
+}
+
+export default Loading;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "#FDF6AA",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30,
+  },
+});
